b33300907bd1290461da76c8fcdcd0d34b0cf371
andrejanev96 <>
2025-08-07 21:36:14 +0200

    corrected vite.config.ts and renamed assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+  import { defineConfig } from 'vite'
+  import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
+  // https://vitejs.dev/config/
+  export default defineConfig({
+    base: '/cartridge-detective-quiz/',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          animations: ['framer-motion'],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['react', 'react-dom'],
+            animations: ['framer-motion'],
+          },
         },
       },
     },
-  },
-})
+  })
